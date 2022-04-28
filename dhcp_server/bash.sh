3988fae27b847e9996fd7bc9d88bbfa4f484b85c
roy8743 <roychavarriagarita@gmail.com>
2022-04-28 15:32:33 -0600

    Cambios en dns y dhcp

--- dhcp_server/bash.sh
+++ dhcp_server/bash.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Initialize the lease file if it doesn't exist.
+touch /data/dhcpd/dhcpd.leases
+
+# Start devpi-server.
+dhcpd -cf /data/dhcpd/dhcpd.conf -lf /data/dhcpd/dhcpd.leases --no-pid -4 -f
+
 /etc/init.d/isc-dhcp-server start
 tail -f /dev/null
+
--- dns/bash.sh
+++ dns/bash.sh
@@ -2,6 +2,7 @@
 #ufw allow bind9
 /etc/init.d/bind9 start
 tail -f /dev/null
+ufw allow Bind9
 #chmod 660 /etc/bind/rndc.conf
 #chown root:bind /etc/bind/rndc.conf
 #cd /etc/dhcp
